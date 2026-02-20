e19389d2f40020d5397c226bc298a2cac47c1a1c
Didar <didarjaff2002@gmail.com>
2026-02-19 19:01:35 -0700

    Created program to demonstrate the importance of hashing and salting passwords.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Block_Project_2
-A small program that demonstrates a security/ethical hacking concept
+This is a simple program to demonstrate hashing passwords. It shows the difference between a salted
+and unsalted hash of a password. Passwords with no salt will always produce the same hash meaning two people can have the same password and they will produce the exact same hash. Salted passwords will always produce a unique hash regardless if two people have the same passwords.
+
+What is salting?
+- It is a technique that adds random data(salt) to a password before it is hashed and stored.
+
+# python setup
+Make sure you have python installed on your device. You can install python off python.org.
+This program was coded in VS Code.
+
+## Dependencies to install
+
+In the terminal, you need install the following:
+
+pip install PyQt6
+pip install bcrypt
+(If these do not work use the two commands below)
+
+python -m pip install PyQt6
+python -m pip install bcrypt
+
+Once installed, you can run the program by typing "python main.py" inside of the terminal. 
+
+You should be prompted to enter in a fake password and two buttons to either show the unsalted hash of that password or the salted hash of that password. The salted hash will produce a new hash each time with the same password inputted and the unsalted will produce the same hash for the same password.
+
+# Warnings
+This tool is for educational use only and should not be used for securing sensitive information. This to simply demonstrate the security of having passwords hashed with salt.
+
+# Ethical considerations and responsible use
+The project shows how passwords are protected and why salting them is important for security. It should not be used with real passwords, it should not be used to try and crack passwords, and it should only be used locally to test fake passwords. No other outside use should be considered, it is just to show the importance of salting.
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,71 @@
+import sys
+import hashlib
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QLineEdit, QTextEdit
+from PyQt6.QtCore import Qt
+import bcrypt
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("PyQt5 Example")
+        self.setGeometry(200, 100, 800, 600)
+
+        label = QLabel("Welcome to the password Hasher! This program demonstrates" \
+        "a salted and unsalted hash for a password. Please enter" 
+        " a test password.", self)
+        label.setAlignment(Qt.AlignmentFlag.AlignTop)
+        label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.setCentralWidget(label)
+
+        self.text_input = QLineEdit(self)
+        self.text_input.setPlaceholderText("Type a test password here")
+        self.text_input.setGeometry(200, 50, 400, 30)
+
+        hash_button = QPushButton("Unsalted Hash", self)
+        hash_button.setGeometry(350, 100, 100, 30)
+        hash_button.clicked.connect(self.print_hash)
+
+        salt_button = QPushButton("Hash with Salt", self)
+        salt_button.setGeometry(350, 150, 100, 30)
+        salt_button.clicked.connect(self.print_salt_hash)
+
+        self.hashes = QTextEdit(self)
+        self.hashes.setGeometry(200, 200, 400, 200)
+        self.hashes.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.hashes.setReadOnly(True)
+
+       
+
+
+    def print_hash(self):
+        password = self.text_input.text()
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        self.hashes.setText(
+            f"Unsalted Hash: {hashed_password}\n\n"
+            "The unsalted hash is a SHA-256 hash of the password you entered. The problem with unsalted hashes is that" 
+            " two users with the same password will have the same hash, which can make it easier for attackers to crack" 
+            " passwords especially if people have common passwords."
+        )
+        
+    def print_salt_hash(self):
+        password = self.text_input.text()
+        saltedPassword = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        self.hashes.setText(f"Salted Hash: {saltedPassword.decode()}\n\n"
+        "The salted hash is a much more secure way to hash a password. It uses the bcrypt library to create a unique salt for each password, " 
+        "which makes it resistant to brute-force attacks and rainbow table attacks. "
+        "Even if two users have the same password, "
+        "their salted hashes will be different due to the unique salt. Try and hash the same password twice to see the different hashes!"
+        )
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":    
+    main()
